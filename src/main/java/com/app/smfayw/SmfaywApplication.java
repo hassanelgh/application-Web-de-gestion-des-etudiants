9254b24987f5aa9f1fede4f7a64c78cfa983d8a5
hassanelgh <elghalbzourihassan1@gmail.com>
2022-04-17 14:15:51 +0000

    la page étudiants

--- src/main/java/com/app/smfayw/SmfaywApplication.java
+++ src/main/java/com/app/smfayw/SmfaywApplication.java
@@ -3,12 +3,13 @@ package com.app.smfayw;
 import com.app.smfayw.entites.Etudiant;
 import com.app.smfayw.entites.Genre;
 import com.app.smfayw.repositories.EtudiantRepository;
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.person.Person;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Date;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -19,11 +20,26 @@ public class SmfaywApplication {
     }
 
 
-    @Bean
+    //@Bean
     CommandLineRunner commandLineRunner(EtudiantRepository etudiantRepository){
         return args -> {
+            // initialiser les données (les etudiants) dans la base de donnée :
 
-            etudiantRepository.save(new Etudiant(UUID.randomUUID().toString(),"el ghalbzouri","hassan","hassan@gmail.com",new Date(), Genre.MASCULIN,true));
+            Fairy fairy = Fairy.create();
+            for (int i = 0; i < 100 ; i++) {
+                Person person = fairy.person();
+                Etudiant etudiant=new Etudiant();
+                etudiant.setId(UUID.randomUUID().toString());
+                etudiant.setNom(person.getLastName());
+                etudiant.setPrenom(person.getFirstName());
+                etudiant.setEmail(person.getEmail());
+                etudiant.setDateNaissance(person.getDateOfBirth().toDate());
+                etudiant.setGenre(person.isFemale()? Genre.FEMININ :Genre.MASCULIN);
+                etudiant.setEnRegle(Math.random() >0.5);
+                etudiantRepository.save(etudiant);
+
+            }
+
 
 
         };
--- src/main/java/com/app/smfayw/repositories/EtudiantRepository.java
+++ src/main/java/com/app/smfayw/repositories/EtudiantRepository.java
@@ -1,7 +1,10 @@
 package com.app.smfayw.repositories;
 
 import com.app.smfayw.entites.Etudiant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EtudiantRepository extends JpaRepository<Etudiant,String> {
+    Page<Etudiant>findEtudiantByNomContains(String cle, Pageable pageable);
 }
